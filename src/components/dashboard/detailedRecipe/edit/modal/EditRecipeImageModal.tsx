be548ccc46092c8ed0e1466c60e362bbd4ef0447
Miguel Bento <miguelbento2000@gmail.com>
2024-07-23 10:03:29 +0200

    edit modal

--- src/components/dashboard/detailedRecipe/edit/modal/EditRecipeImageModal.tsx
+++ src/components/dashboard/detailedRecipe/edit/modal/EditRecipeImageModal.tsx
@@ -11,7 +11,7 @@ const EditRecipeImageModal: React.FC<IProps> = ({ recipe }) => {
     return (
         <Modal
             modalId={RECIPE_ACTION_MODAL_IDS.EDIT_IMAGE}
-            extraModalBoxClasses="flex flex-col justify-start items-start gap-2"
+            extraModalBoxClasses="flex flex-col justify-start items-start gap-2 bg-base-300 max-h-screen"
         >
             {/* TITLE */}
             <div className="text-xl text-white font-bold">Edit Image</div>
@@ -26,9 +26,13 @@ const EditRecipeImageModal: React.FC<IProps> = ({ recipe }) => {
             <img
                 src={recipe.image ?? recipePlaceholderImg}
                 alt="Recipe image"
+                className="border bg-base-100 rounded-md border-gray-600"
             />
 
             {/* SELECT IMAGE BUTTON */}
+            <button className="btn w-full common_btn">
+                Upload Image
+            </button>
 
             {/* SMALL DISCLAIMER */}
         </Modal>
